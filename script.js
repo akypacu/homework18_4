c3d5ad0299576d65826f1f0a43bfbc7f7c457bb0
akypacu <aleksejkirmel20@gmail.com>
2023-04-15 16:26:14 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const getData = async (url) => {
+    const res = await fetch(url)
+    const data = res.json()
+    return data
+}
+const postData = async (url, obj) => {
+    const res = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(obj),
+        headers: { 'Content-type': 'application/json; charset=UTF-8' },
+    })
+    return res.json()
+}
+const URL = "http://localhost:3000/USERS"
+const buttonPost = document.querySelector('.setBtn')
+const buttonGet = document.querySelector('.getBtn')
+const users = document.querySelector('.users')
+buttonPost.addEventListener('click', async (e) => {
+    e.preventDefault();
+    const id = document.querySelector('input.id').value
+    const name = document.querySelector('input.name').value
+    const age = document.querySelector('input.age').value
+    const gender = document.querySelector('input.gender').value
+    let obj = {
+        "id": id,
+        "name": name,
+        "age": age,
+        "gender": gender
+    }
+    await postData(URL, obj)
+})
+buttonGet.addEventListener('click', async (e) => {
+    e.preventDefault()
+    const data = await getData(URL)
+    data.forEach(el => {
+        users.insertAdjacentHTML(`beforeend`, `
+            <div class="user">
+                <p class="id">id:${el.id}</p>
+                <p class="name">name:${el.name}</p>
+                <p class="age">age:${el.age}</p>
+                <p class="gender">gender:${el.gender}</p>
+            </div>
+        `)
+    })
+})
